9b683227cb7e794b51b147c69552d804759062c0
Fanny Nina Paravecino <fninaparavecino@gmail.com>
2017-02-10 15:22:46 -0500

    Add verify function

--- recApps/fib/src/fib.cpp
+++ recApps/fib/src/fib.cpp
@@ -49,30 +49,48 @@ long long int fib(int n){
         return fib(n-1) + fib(n-2);
     }
 }
-long long int fibSeq(int n){
-    long long int n0 = 0;
-    long long int n1 = 1;
-    if (n == 0 || n == 1)
-        return n;
-    long long int nTmp;
-    for (int i = 2; i < n; i++){
-        nTmp = n1;
-        n1 = n1 + n0;
-        n0 = nTmp;
+long long int fibSeq(int n, long int *arrayNSeq){
+
+    for (int i = 2; i <= n; i++){
+      arrayNSeq[i] = arrayNSeq[i-1]+ arrayNSeq[i-2];
     }
-    return n1 + n0;
+    return arrayNSeq[n];
+}
+void verifyArrays(int n, long int *array1, long int *array2, const char *message)
+{
+	int flag = 1;
+	for (int i=0; i < n; i++){
+		if (array1[i]!=array2[i]){
+			printf("ERROR: validation error at %d: %s !\n", i, message);
+			printf("Index %d : %ld vs %ld\n", i, array1[i], array2[i]);
+			flag = 0; break;
+		}
+	}
+	if (flag) printf("PASS: %s !\n", message);
 }
 
-int main(int argc, char** argv){
-    int n = 0;
-    if ( !parse_arguments(argc, argv, &n) ) return 0;
-    printf("Computing fib(%d)\n", n);
-    printf("Fib of %d: %lld\n", n, fib(n));
-    printf("Fib Seq of %d: %lld\n", n, fibSeq(n));
 
+int main(int argc, char** argv){
+
+    int n = 0;
+
+    // seeds
+    long int arraySeed[4] = {0, 1, 2178309, 3524578};
+
+    if ( !parse_arguments(argc, argv, &n) ) return 0;
+
+    printf("Computing fib(%d)\n", n);
+
+    // printf("Fib of %d: %lld\n", n, fib(n)); // Commenting out, due to it is taking long time to compute
+
+    long int* arrayNSeq = (long int*)malloc(sizeof(long int) * (n+1));
+    memset(arrayNSeq, 0, (n+1)*sizeof(long int));
+    arrayNSeq[0] = 0; arrayNSeq[1] = 1;
+
+    fibSeq(n, arrayNSeq);
+    printf("Fib Seq of %d: %ld\n", n, arrayNSeq[n]);
 
     // Call GPU kernel
-    long int arraySeed[4] = {0, 1, 2178309, 3524578};
     long int* arrayN = (long int*)malloc((n+1)*sizeof(long int));
     memset(arrayN, 0, (n+1)*sizeof(long int));
     int auxIndex = 0;
@@ -84,5 +102,9 @@ int main(int argc, char** argv){
     fibGPU(n, arrayN);
 
     printf("Fib GPU of %d: %ld\n", n, arrayN[n]);
+
+    verifyArrays(n+1, arrayNSeq, arrayN, "CPU vs GPU");
+
+    free(arrayN); free(arrayNSeq);
     return 0;
 }
